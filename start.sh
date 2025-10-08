cf1e46f67693b9e8fb1e9f25f0869dd83b146259
ahmedafzal98 <ahmed.afzal2070@gmail.com>
2025-10-08 23:15:51 +0500

    update start.sh

--- start.sh
+++ start.sh
@@ -4,5 +4,8 @@ set -e
 # Install Tesseract OCR
 apt-get update && apt-get install -y tesseract-ocr
 
-# Start FastAPI server
+# Move into the server directory
+cd server
+
+# Run your FastAPI app
 exec uvicorn main:app --host 0.0.0.0 --port 10000
